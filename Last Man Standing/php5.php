e1255ad932bd4c706c175ef6f43d57461e8643f2
Amal A C <acboy5526@gmail.com>
2019-03-17 21:30:19 +0530

    Add files via upload

---
+++ Last Man Standing/php5.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password= "";
+$dbname = "lastmanstanding";
+$conn =new \MySQLi($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn_error);
+}
+$result=mysqli_query($conn,"SELECT Q4,total FROM answers");
+$row=mysqli_fetch_assoc($result);
+$p=$row['Q4'];
+$q=$row['total'];
+echo $p;
+if($_POST["pswrd"] == $p)
+{
+mysqli_query($conn,"UPDATE progress SET Q4=1,total=total+1 WHERE TeamID={$_SESSION['user_id']}");
+if($q==5);
+{
+	header('Location: KILL.html');
+}
+else
+header('Location: Questions.html');
+}
+else{
+	$_SESSION['error_message'] = 'Wrong Password';
+header('Location: question4.html');
+}
+?>
