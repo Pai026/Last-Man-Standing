b79f2360756fb12b4c87ccd5702051a069b7b8e6
Abhiram Pai <36255896+Pai026@users.noreply.github.com>
2019-03-17 20:37:18 +0530

    Add files via upload

--- Last Man Standing/php2.php
+++ Last Man Standing/php2.php
@@ -9,13 +9,19 @@ $conn =new \MySQLi($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn_error);
 }
-$result=mysqli_query($conn,"SELECT Q1 FROM answers");
+$result=mysqli_query($conn,"SELECT Q1,total FROM answers");
 $row=mysqli_fetch_assoc($result);
 $p=$row['Q1'];
+$q=$row['total'];
 echo $p;
 if($_POST["pswrd"] == $p)
 {
-mysqli_query($conn,"UPDATE progress SET Q1=1 WHERE TeamID={$_SESSION['user_id']}");
+mysqli_query($conn,"UPDATE progress SET Q1=1,total=total+1 WHERE TeamID={$_SESSION['user_id']}");
+if($q==5);
+{
+	header('Location: KILL.html');
+}
+else
 header('Location: Questions.html');
 }
 
