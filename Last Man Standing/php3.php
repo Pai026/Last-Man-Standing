78617f1a7bd16d33d70b1c37abfa11b3d94471e3
Abhiram Pai <36255896+Pai026@users.noreply.github.com>
2019-03-18 14:25:06 +0530

    Update php3.php

--- Last Man Standing/php3.php
+++ Last Man Standing/php3.php
@@ -9,6 +9,7 @@ $conn =new \MySQLi($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn_error);
 }
+session_start();
 $result=mysqli_query($conn,"SELECT Q2,total FROM answers");
 $row=mysqli_fetch_assoc($result);
 $p=$row['Q2'];
@@ -17,7 +18,7 @@ echo $p;
 if($_POST["pswrd"] == $p)
 {
 mysqli_query($conn,"UPDATE progress SET Q2=1,total=total+1 WHERE TeamID={$_SESSION['user_id']}");
-if($q==5);
+if($q==5)
 {
 	header('Location: KILL.html');
 }
