1bc7e3925dd1f29a049c0cbc304299eaa8802958
Abhiram Pai <36255896+Pai026@users.noreply.github.com>
2019-03-19 22:03:17 +0530

    Add files via upload

--- Last Man Standing/php3.php
+++ Last Man Standing/php3.php
@@ -10,18 +10,20 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn_error);
 }
 session_start();
-$result=mysqli_query($conn,"SELECT Q2,total FROM answers");
-$row=mysqli_fetch_assoc($result);
-$p=$row['Q2'];
-$q=$row['total'];
-echo $p;
 $r=$_SESSION['user_id'];
+$result=mysqli_query($conn,"SELECT Q2 FROM answers");
+$result1=mysqli_query($conn,"SELECT total FROM progress where TeamID=$r");
+$row=mysqli_fetch_assoc($result);
+$row1=mysqli_fetch_assoc($result1);
+$p=$row['Q5'];
+$q=$row1['total'];
+echo $p;
 if($_POST["pswrd"] == $p)
 {
 mysqli_query($conn,"UPDATE progress SET Q2=1,total=total+1 WHERE TeamID=$r");
 if($q+1==5)
 {
-	mysqli_query($conn,"UPDATE teams SET timestamp=CURRENT_TIMESTAMP() WHERE TeamID=$r");
+	mysqli_query($conn,"UPDATE teams SET timestampf=CURRENT_TIMESTAMP() WHERE TeamID=$r");
 	header('Location: KILL.html');
 	 unset($_SESSION['user_id']);
  session_destroy();
--- Last Man Standing/php7.php
+++ Last Man Standing/php7.php
@@ -3,26 +3,26 @@ $servername = "localhost";
 $username = "root";
 $password= "";
 $dbname = "lastmanstanding";
-
 $conn =new \MySQLi($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn_error);
 }
 session_start();
 $r=$_SESSION['user_id'];
-$result=mysqli_query($conn,"SELECT Q1 FROM answers");
+$result=mysqli_query($conn,"SELECT Q5 FROM answers");
 $result1=mysqli_query($conn,"SELECT total FROM progress where TeamID=$r");
 $row=mysqli_fetch_assoc($result);
 $row1=mysqli_fetch_assoc($result1);
 $p=$row['Q5'];
+echo $p;
 $q=$row1['total'];
 echo $p;
 if($_POST["pswrd"] == $p)
 {
-mysqli_query($conn,"UPDATE progress SET Q1=1,total=total+1 WHERE TeamID=$r");
+mysqli_query($conn,"UPDATE progress SET Q5=1,total=total+1 WHERE TeamID=$r");
 if($q+1==5)
 {
+	
 	mysqli_query($conn,"UPDATE teams SET timestampf=CURRENT_TIMESTAMP() WHERE TeamID=$r");
 	header('Location: KILL.html');
 	 unset($_SESSION['user_id']);
@@ -31,10 +31,8 @@ if($q+1==5)
 else
 header('Location: Questions.html');
 }
-
-
 else{
 	$_SESSION['error_message'] = 'Wrong Password';
-header('Location: question1.html');
+header('Location: Questions.html');
 }
 ?>
